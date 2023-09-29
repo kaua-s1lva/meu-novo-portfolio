1375631318213772cde6f3ab840d35580ecfc000
kaua <kaua.jesuscristo78@gmail.com>
2023-09-29 01:09:37 -0300

    corrigindo aspas

--- src/components/SectionAbout.tsx
+++ src/components/SectionAbout.tsx
@@ -6,14 +6,14 @@ export function SectionAbout () {
             <Container>
                 <div className="text-white text-center w-[80%] m-auto mt-[160px] mb-[260px]">
                     <h1 className="text-[25px] font-bold mb-6">Olá, me chamo Kauã. Muito prazer!</h1>
-                    <h2 className="text-[21px] ">
+                    <h2 className="text-[21px]">
                     Meu primeiro contato com a programação foi em 2021, quando ainda estava no ensino médio, 
                     trabalhando como estagiário no setor financeiro, quando conheci o gerente de TI da empresa, 
                     ao qual ele me instigou a conhecer melhor a área da tecnologia, onde realizei projetos 
                     utilizando o PowerBI para otimizar e aprimorar meus trabalhos. Apesar de ter gostado da 
                     área, só comecei a estudar desenvolvimento web em 2022. 
                     <br />
-                    Hoje já entendo de tecnologias voltadas para o Front-end e Back-end. Também gosto de "brincar" 
+                    Hoje já entendo de tecnologias voltadas para o Front-end e Back-end. Também gosto de &quot;brincar&quot; 
                     com design no figma, criando interfaces para projetos pessoais e de amigos.
                     </h2>
                 </div>
